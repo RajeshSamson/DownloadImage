31fc01f80961cec5cbe380d60243c92b8dd5414d
rajeshsamson <rj.drummerboy@gmail.com>
2019-02-21 17:38:42 +0530

    modified code to download the file

--- src/main/java/com/example/downloadimage/resource/ImageResource.java
+++ src/main/java/com/example/downloadimage/resource/ImageResource.java
@@ -1,27 +1,46 @@
 package com.example.downloadimage.resource;
 
-import com.example.downloadimage.model.Image;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/app")
 public class ImageResource {
 
-    private byte[] data;
+    private static final String EXTENSION = ".jpg";
+    private static final String SERVER_LOCATION = "/server/images";
 
+    @RequestMapping(path = "/download", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download(@RequestParam("image") String image) throws IOException {
+        File file = new File(SERVER_LOCATION + File.separator + image + EXTENSION);
 
-    @GetMapping(value = "/img")
-    public Image getImage() {
-        return null;
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.jpg");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
     }
+
 }
